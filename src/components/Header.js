de6488de7843015bcd494d24098a45a833e98e9f
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-15 15:48:10 +0530

    git init

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-2xl text-red-500 font-bold p-3">
+      Filmy<span className="text-white ">Verse</span>{" "}
+    </div>
+  );
+};
+
+export default Header;
