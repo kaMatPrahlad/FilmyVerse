6ef1fb2cb0821569319c01170af1508cd7b978b7
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-17 14:11:47 +0530

    git init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import AddIcon from "@mui/icons-material/Add";
 
 const Header = () => {
   return (
-    <div className="text-2xl text-red-500 font-bold p-3">
-      Filmy<span className="text-white ">Verse</span>{" "}
+    <div className="text-2xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500">
+      <span>
+        Filmy<span className="text-white ">Verse</span>
+      </span>
+      <h1 className="text-lg text-white cursor-pointer flex items-center">
+        <AddIcon className="mr-2" color="inherit" />
+        Add New
+      </h1>
     </div>
   );
 };
