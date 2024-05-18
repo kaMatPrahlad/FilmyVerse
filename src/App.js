589308a38fa6b0db53ed70a0d30b9c63c9ea5092
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-18 16:28:53 +0530

    git init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import AddMovie from "./components/AddMovie";
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Cards />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="addmovie" element={<AddMovie />} />
+      </Routes>
     </div>
   );
 }
