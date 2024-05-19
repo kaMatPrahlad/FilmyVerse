c9222c35c9a1c950d9ee3762bc4d21e3be02dbf1
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-19 13:12:55 +0530

    git inti

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBssCRPt4t7Nv-3SjBmRCbQeO37qYvcgkU",
+  authDomain: "filmyverse-2c66d.firebaseapp.com",
+  projectId: "filmyverse-2c66d",
+  storageBucket: "filmyverse-2c66d.appspot.com",
+  messagingSenderId: "498805508106",
+  appId: "1:498805508106:web:c199a3fb381d5b0fdab7f3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
