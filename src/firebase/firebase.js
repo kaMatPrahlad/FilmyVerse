bce35cc6006146216dc44b49aa44a73d45db0c27
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-23 14:54:42 +0530

    git init

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBssCRPt4t7Nv-3SjBmRCbQeO37qYvcgkU",
   authDomain: "filmyverse-2c66d.firebaseapp.com",
@@ -11,5 +11,7 @@ const firebaseConfig = {
   appId: "1:498805508106:web:c199a3fb381d5b0fdab7f3",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export default app;
