fbc43bd3f5551e94e8156eff596d5981675f2889
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-24 16:28:11 +0530

    git init

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  return (
+    <div className="p-4 flex justify-center">
+      <img src="" />
+    </div>
+  );
 };
 
 export default Detail;
