bfcfb24d7688c4e223437eecf3416ba39dd56466
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-24 13:49:58 +0530

    "Updated package-lock.json, package.json, and various JavaScript files to add sweetalert and promise-polyfill dependencies, modified AddMovie component, and made changes to Header and index.css files."

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,12 +1,30 @@
 import { Description } from "@mui/icons-material";
 import React, { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import {addDoc} from "firebase/firestore"
+import {moviesRef} from "../firebase/firebase"
+import swal from 'sweet-alert'
 
 const AddMovie = () => {
   const [form, setForm] = useState({
     title: "",
     Year: "",
     Description: "",
+    image: "",
   });
+  const [loading, setLoading] = useState(false);
+
+  const addMovie = async ()=>{
+    await addDoc(moviesRef,form);
+    swal({
+      title: "Successfully Added",
+      icon: "succcess",
+      buttons:false,
+      timer:3000,
+
+    })
+  }
+
   return (
     <div>
       <section className="text-gray-600 body-font relative">
@@ -21,7 +39,7 @@ const AddMovie = () => {
             <div className="flex flex-wrap -m-2">
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label for="name" className="leading-7 text-sm text-gray-600">
+                  <label htmlFor="name" className="text-sm text-gray-300">
                     Title
                   </label>
                   <input
@@ -38,7 +56,10 @@ const AddMovie = () => {
               </div>
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label for="email" className="leading-7 text-sm text-gray-300">
+                  <label
+                    for="email"
+                    className="leading-7 text-sm text-gray-300"
+                  >
                     Year
                   </label>
                   <input
@@ -53,23 +74,45 @@ const AddMovie = () => {
               </div>
               <div className="p-2 w-full">
                 <div className="relative">
-                  <label for="message" className="leading-7 text-sm text-gray-600">
+                  <label
+                    for="message"
+                    className="leading-7 text-sm text-gray-300"
+                  >
+                    Image Link
+                  </label>
+                  <input
+                    id="message"
+                    name="message"
+                    value={form.image}
+                    onChange={(e) =>
+                      setForm({ ...form, image: e.target.value })
+                    }
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    for="message"
+                    className="leading-7 text-sm text-gray-300"
+                  >
                     Description
                   </label>
-                  <textarea
-                    id="message"
+                  <input
+                    id="description"
                     name="message"
                     value={form.Description}
                     onChange={(e) =>
                       setForm({ ...form, Description: e.target.value })
                     }
                     className="w-full bg-white  rounded border border-gray-300 focus:border-indigo-500  focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
+                  />
                 </div>
               </div>
               <div className="p-2 w-full">
-                <button className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
-                  Submit
+                <button oncli className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
+                  {loading ? <TailSpin height={25} color="white" /> : "Submit"}
                 </button>
               </div>
             </div>
