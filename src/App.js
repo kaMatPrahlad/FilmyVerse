a64e8e1fe12d4528f195a6451a5732dde6fc55cd
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-25 15:07:36 +0530

    git init

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import Cards from "./components/Cards";
 import Detail from "./components/Detail";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+
+const Appstate = createContext();
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+
   return (
-    <div className="App relative">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Cards />} />
-        <Route path="addmovie" element={<AddMovie />} />
-        <Route path="detail/:id" element={<Detail />} />
-      </Routes>
-    </div>
+    <Appstate.Provider value={(login, userName, setLogin)}>
+      <div className="App relative">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="addmovie" element={<AddMovie />} />
+          <Route path="detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+    </Appstate.Provider>
   );
 }
 
-export default App;
+export default App(Appstate);
