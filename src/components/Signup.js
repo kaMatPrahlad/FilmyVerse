a70c523d34109df40252ae5fde09b56fda9b0998
kaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-27 14:30:23 +0530

    git init

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,7 +15,6 @@ import bcrypt from "bcryptjs";
 const auth = getAuth(app);
 
 const Signup = () => {
-  //Signup info
   const navigate = useNavigate();
   const [form, setForm] = useState({
     name: "",
@@ -98,12 +97,9 @@ const Signup = () => {
       <h1 className="text-xl font-bold">Sign up</h1>
       {otpSent ? (
         <>
-          <div className="p-2 w-full md:w-1/3">
-            <div className="relative">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-300"
-              >
+          <div class="p-2 w-full md:w-1/3">
+            <div class="relative">
+              <label for="message" class="leading-7 text-sm text-gray-300">
                 OTP
               </label>
               <input
@@ -111,14 +107,14 @@ const Signup = () => {
                 name="message"
                 value={OTP}
                 onChange={(e) => setOTP(e.target.value)}
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
           </div>
-          <div className="p-2 w-full">
+          <div class="p-2 w-full">
             <button
               onClick={verifyOTP}
-              className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
+              class="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
             >
               {loading ? <TailSpin height={25} color="white" /> : "Confirm OTP"}
             </button>
@@ -126,12 +122,9 @@ const Signup = () => {
         </>
       ) : (
         <>
-          <div className="p-2 w-full md:w-1/3">
-            <div className="relative">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-300"
-              >
+          <div class="p-2 w-full md:w-1/3">
+            <div class="relative">
+              <label for="message" class="leading-7 text-sm text-gray-300">
                 Name
               </label>
               <input
@@ -139,16 +132,13 @@ const Signup = () => {
                 name="message"
                 value={form.name}
                 onChange={(e) => setForm({ ...form, name: e.target.value })}
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
           </div>
-          <div className="p-2 w-full md:w-1/3">
-            <div className="relative">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-300"
-              >
+          <div class="p-2 w-full md:w-1/3">
+            <div class="relative">
+              <label for="message" class="leading-7 text-sm text-gray-300">
                 Mobile No.
               </label>
               <input
@@ -157,16 +147,13 @@ const Signup = () => {
                 name="message"
                 value={form.mobile}
                 onChange={(e) => setForm({ ...form, mobile: e.target.value })}
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
           </div>
-          <div className="p-2 w-full md:w-1/3">
-            <div className="relative">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-300"
-              >
+          <div class="p-2 w-full md:w-1/3">
+            <div class="relative">
+              <label for="message" class="leading-7 text-sm text-gray-300">
                 Password
               </label>
               <input
@@ -175,14 +162,14 @@ const Signup = () => {
                 name="message"
                 value={form.password}
                 onChange={(e) => setForm({ ...form, password: e.target.value })}
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
           </div>
-          <div className="p-2 w-full">
+          <div class="p-2 w-full">
             <button
               onClick={requestOtp}
-              className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
+              class="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
             >
               {loading ? <TailSpin height={25} color="white" /> : "Request OTP"}
             </button>
